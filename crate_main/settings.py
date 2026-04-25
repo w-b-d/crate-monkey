89aeb6ab20a1a60c65458a64af599306407ce0ff
will <161429870+w-b-d@users.noreply.github.com>
2026-04-25 15:27:53 -0700

    update
    Remove the secret key for security reasons.

--- crate_main/settings.py
+++ crate_main/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#en7-7@3v#7c%_bj^&$q$scw!%2dam4qvxzb^hlt5c0eez=%0y"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
